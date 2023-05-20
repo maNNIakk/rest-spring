526ecbb514b9d9cadcd74530c24bfa91d3f16fda
Renato <renato.yancovit3@gmail.com>
2023-05-20 18:37:35 -0300

    Finalizando a secao13/14

--- 05_FirstStepsInJavawithSpringBoot/rest-spring/src/main/java/br/com/restspring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-spring/src/main/java/br/com/restspring/exceptions/handler/CustomizedResponseEntityExceptionHandler.java
@@ -11,6 +11,7 @@ import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import br.com.restspring.exceptions.ExceptionResponse;
+import br.com.restspring.exceptions.RequiredObjectIsNullException;
 import br.com.restspring.exceptions.ResourceNotFoundException;
 
 @ControllerAdvice
@@ -28,6 +29,12 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 			Exception ex,WebRequest request) {
 		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
 		return new ResponseEntity<>(exceptionResponse, HttpStatus.NOT_FOUND);
+	}
+	@ExceptionHandler(RequiredObjectIsNullException.class)
+	public final ResponseEntity<ExceptionResponse> handleBadRequestException(
+			Exception ex,WebRequest request) {
+		ExceptionResponse exceptionResponse = new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<>(exceptionResponse, HttpStatus.BAD_REQUEST);
 	}
 	
 }
--- 05_FirstStepsInJavawithSpringBoot/rest-spring/src/main/java/br/com/restspring/services/PersonServices.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-spring/src/main/java/br/com/restspring/services/PersonServices.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 
 import br.com.restspring.controllers.PersonController;
 import br.com.restspring.data.vo.v1.PersonVO;
+import br.com.restspring.exceptions.RequiredObjectIsNullException;
 import br.com.restspring.exceptions.ResourceNotFoundException;
 import br.com.restspring.mapper.DozerMapper;
 import br.com.restspring.model.Person;
@@ -17,9 +18,9 @@ import br.com.restspring.repositories.PersonRepository;
 
 @Service
 public class PersonServices {
-	
+
 	private Logger logger = Logger.getLogger(PersonServices.class.getName());
-	
+
 	@Autowired
 	PersonRepository repository;
 
@@ -28,51 +29,55 @@ public class PersonServices {
 		logger.info("Finding all people!");
 
 		var persons = DozerMapper.parseListObjects(repository.findAll(), PersonVO.class);
-		persons.stream().forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
+		persons.stream()
+				.forEach(p -> p.add(linkTo(methodOn(PersonController.class).findById(p.getKey())).withSelfRel()));
 		return persons;
 	}
 
 	public PersonVO findById(Long id) {
 		
 		logger.info("Finding one person!");
-		
+
 		var entity = repository.findById(id)
-			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		var vo = DozerMapper.parseObject(entity, PersonVO.class);
 		vo.add(linkTo(methodOn(PersonController.class).findById(id)).withSelfRel());
 		return vo;
 	}
-	
+
 	public PersonVO create(PersonVO person) {
 
+		if (person == null) throw new RequiredObjectIsNullException();
 		logger.info("Creating one person!");
 		var entity = DozerMapper.parseObject(person, Person.class);
-		var vo =  DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
 		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
+
 	}
-	
+
 	public PersonVO update(PersonVO person) {
-		
+
+		if (person == null) throw new RequiredObjectIsNullException();
 		logger.info("Updating one person!");
-		
+
 		var entity = repository.findById(person.getKey())
-			.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
+				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 
 		entity.setFirstName(person.getFirstName());
 		entity.setLastName(person.getLastName());
 		entity.setAddress(person.getAddress());
 		entity.setGender(person.getGender());
-		
-		var vo =  DozerMapper.parseObject(repository.save(entity), PersonVO.class);
+
+		var vo = DozerMapper.parseObject(repository.save(entity), PersonVO.class);
 		vo.add(linkTo(methodOn(PersonController.class).findById(vo.getKey())).withSelfRel());
 		return vo;
 	}
-	
+
 	public void delete(Long id) {
-		
+
 		logger.info("Deleting one person!");
-		
+
 		var entity = repository.findById(id)
 				.orElseThrow(() -> new ResourceNotFoundException("No records found for this ID!"));
 		repository.delete(entity);
