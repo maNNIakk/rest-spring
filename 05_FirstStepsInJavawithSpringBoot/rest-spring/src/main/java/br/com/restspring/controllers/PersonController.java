0807890096e71851159d7f88ee7490132f4d6b91
Renato <renato.yancovit3@gmail.com>
2023-05-28 19:06:25 -0300

    Finalizada secao16 - Implementação de CORS e testes de componentes para validação positiva e negativa do CORS

--- 05_FirstStepsInJavawithSpringBoot/rest-spring/src/main/java/br/com/restspring/controllers/PersonController.java
+++ 05_FirstStepsInJavawithSpringBoot/rest-spring/src/main/java/br/com/restspring/controllers/PersonController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,6 +44,7 @@ public class PersonController {
 		return service.findAll();
 	}
 
+	@CrossOrigin(origins = "http://localhost:8080")
 	@GetMapping(value = "/{id}", produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
 			MediaType.APPLICATION_YML })
 	@Operation(summary = "Find a Person", description = "Finds a Person", tags = { "People" }, responses = {
@@ -57,6 +59,7 @@ public class PersonController {
 		return service.findById(id);
 	}
 
+	@CrossOrigin(origins = {"http://localhost:8080","https://erudio.com.br"})
 	@PostMapping(consumes = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
 			MediaType.APPLICATION_YML }, produces = { MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML,
 					MediaType.APPLICATION_YML })
